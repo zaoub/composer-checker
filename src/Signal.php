8eb1d1c205a4f28a22f8f5e1600cb239dae4cf91
Karim Mohamed <komicho1996@gmail.com>
2019-10-11 19:30:19 +0200

    Create signal module

--- src/Signal.php
+++ src/Signal.php
@@ -4,15 +4,13 @@ namespace Zaoub\Dependo;
 
 use Zaoub\Dependo\Config;
 use Zaoub\Dependo\Core\Report;
-use Zaoub\Dependo\SendResults;
 
 use SensioLabs\Security\SecurityChecker;
 
-class Scan
+class Signal
 {
-    public function __construct($options = []) {
-        $this->config = new Config($options);
-        $this->send = new SendResults;
+    public function __construct() {
+        $this->config = new Config([]);
         $this->report = new Report($this->config);
     }
 
@@ -23,17 +21,11 @@ class Scan
      */
     public function run()
     {
-        if ($this->config['type_results'] == 'text') {
-            echo "\e[1;33mChecking... \e[0m".PHP_EOL;
-        }
-
         $checker = new SecurityChecker();
         $result = $checker->check($this->config['composer_lock_path'], 'json');
         $alerts = json_decode((string) $result, true);
         
         $this->report->setData($alerts);
-        echo $this->report->run($this->config['type_results']);
-
-        $this->send->run($this->config);
+        echo $this->report->run('json');
     }
 }
